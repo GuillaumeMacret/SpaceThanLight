948952561a9b8022bc749b08038c3bb0d7a7b2e5
Guillaume Macret <norcis01@hotmail.fr>
2020-08-15 11:27:45 +0200

    :tada: We are au début

---
+++ Assets/Scripts/SpaceShipWeapon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpaceShipWeapon : MonoBehaviour
+{
+    private static readonly int DEFAULT_WEAPON_DAMAGE = 1;
+    private static readonly float DEFAULT_WEAPON_COOLDOWN = 5;
+
+    public int damages = DEFAULT_WEAPON_DAMAGE;
+    public float cooldown = DEFAULT_WEAPON_COOLDOWN;
+    public float cooldownTimer = DEFAULT_WEAPON_COOLDOWN;
+
+    public SpaceShipRoom target;
+
+    private void Update()
+    {
+        if(target != null && cooldownTimer <= 0)
+        {
+            Fire();
+        }
+        cooldownTimer = Mathf.Clamp(cooldownTimer - Time.deltaTime,0,cooldown);
+    }
+
+    private void Fire()
+    {
+        target.Damage(damages);
+        cooldownTimer = cooldown;
+    }
+
+}
---
+++ Assets/Scripts/Spaceship.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+    public SpaceShipRoom[] m_Rooms;
+
+    private void Awake()
+    {
+        m_Rooms = GetComponentsInChildren<SpaceShipRoom>();
+    }
+
+    public int health;
+    public int damages;
+
+    public void Damage(int value)
+    {
+        damages += value;
+    }
+
+}
